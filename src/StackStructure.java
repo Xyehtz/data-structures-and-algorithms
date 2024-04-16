c6101d6311b2eb09b081b51474fe65c1a069ca8f
Xyehtz <alejitogarbo@gmail.com>
2024-04-16 14:26:05 -0400

    init

---
+++ src/StackStructure.java
@@ -0,0 +1,11 @@
+public class Stack {
+    /* Stack: Is a LIFO data structure, this means that it is:
+        - First in, last out
+    It stores the values in a vertical tower, each value added goes to the top
+    The methods to add and remove values are:
+        - .push() to add
+        - .pop() to remove
+     */
+
+    Stack<String> gamesStack = new Stack<String>();
+}
